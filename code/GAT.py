0af3e83d8a5ba7883d2821df441a8d1f07b1bade
zincalex <92792074+zincalex@users.noreply.github.com>
2024-12-27 12:33:39 +0100

    GAT model class

--- code/GAT.py
+++ code/GAT.py
@@ -27,6 +27,138 @@ import numpy as np
 import tensorflow as tf
 import networkx as nx
 import os
+from tensorflow import keras
+from tensorflow.keras import layers, Model
+
+class GraphAttentionLayer(layers.Layer):
+
+    # units is number of output features for each node after the attention mechanism
+    # kernel_init Specifies the method for initializing weights
+    # regula Optional regularization to apply to the weights, which can help prevent overfitting
+    def __init__(self, units, kernel_initializer="glorot_uniform", regularizer=None, **kwargs):
+        super().__init__(**kwargs)
+        self.units = units
+        self.kernel_initializer = tf.keras.initializers.get(kernel_initializer)
+        self.regularizer = tf.keras.regularizers.get(regularizer)
+
+    # Creates the learnable weights for the layer
+    # input_shape The shape of the input tensors.
+    def build(self, input_shape):
+        self.kernel = self.add_weight(
+            shape=(input_shape[0][-1], self.units),
+            trainable=True,
+            initializer=self.kernel_initializer,
+            regularizer=self.regularizer,
+            name="kernel",
+        )
+
+        # Used to compute attention scores between node pairs
+        #  factor of 2 accounts for concatenating the features of the source and destination nodes during attention calculation
+        self.kernel_attention = self.add_weight(
+            shape=(self.units * 2, 1),
+            trainable=True,
+            initializer=self.kernel_initializer,
+            regularizer=self.regularizer,
+            name="kernel_attention",
+        )
+        self.built = True
+
+    # forward pass logic of the attention layer
+    def call(self, inputs):
+
+        # node_states is the feature vector of all nodes in the graph
+        # edges has in each row a pair of node indices defining an edge (source, target)
+        node_states, edges = inputs
+
+        # TOKENIZATION & EMBEDDING 
+        node_states_transformed = tf.matmul(node_states, self.kernel)
+
+        # ATTENTION SCORES
+        node_states_expanded = tf.gather(node_states_transformed, edges)
+        node_states_expanded = tf.reshape(node_states_expanded, (tf.shape(edges)[0], -1))
+        # compute raw attention scores for each edge, and then activate using a Leaky ReLU function
+        attention_scores = tf.nn.leaky_relu(tf.matmul(node_states_expanded, self.kernel_attention))     
+        attention_scores = tf.squeeze(attention_scores, -1)                                             
+
+        # NORMALIZE 
+        attention_scores = tf.math.exp(tf.clip_by_value(attention_scores, -2, 2)) # TODO CARE ABOUT THIS CLIPPING VALUES
+        attention_scores_sum = tf.math.unsorted_segment_sum(
+            data=attention_scores,
+            segment_ids=edges[:, 0],
+            num_segments=tf.reduce_max(edges[:, 0]) + 1,
+        )
+        attention_scores_sum = tf.repeat(
+            attention_scores_sum, tf.math.bincount(tf.cast(edges[:, 0], "int32"))
+        )
+        attention_scores_norm = attention_scores / attention_scores_sum
+
+        # ADD ----> Aggregate neighbor states
+        node_states_neighbors = tf.gather(node_states_transformed, edges[:, 1])
+        out = tf.math.unsorted_segment_sum(
+            data=node_states_neighbors * attention_scores_norm[:, tf.newaxis],
+            segment_ids=edges[:, 0],
+            num_segments=tf.shape(node_states)[0],
+        )
+        return out
+
+# Multi-Head Graph Attention Layer
+class MultiHeadGraphAttentionLayer(layers.Layer):
+    def __init__(self, units, num_heads=4, merge_type="concat", **kwargs):
+        super().__init__(**kwargs)
+        self.num_heads = num_heads
+        self.merge_type = merge_type
+        self.attention_layers = [GraphAttentionLayer(units) for _ in range(num_heads)]
+
+    def call(self, inputs):
+        atom_features, pair_indices = inputs
+        outputs = [attention_layer([atom_features, pair_indices]) for attention_layer in self.attention_layers]
+        if self.merge_type == "concat":
+            outputs = tf.concat(outputs, axis=-1)
+        else:
+            outputs = tf.reduce_mean(tf.stack(outputs, axis=-1), axis=-1)
+        return tf.nn.relu(outputs)
+
+# Graph Attention Network Model
+class GraphAttentionNetwork(Model):
+    def __init__(self, node_states, edges, hidden_units, num_heads, num_layers, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.node_states = node_states
+        self.edges = edges
+        self.preprocess = layers.Dense(hidden_units * num_heads, activation="relu")
+        self.attention_layers = [
+            MultiHeadGraphAttentionLayer(hidden_units, num_heads) for _ in range(num_layers)
+        ]
+        self.output_layer = layers.Dense(output_dim)
+
+    def call(self, inputs):
+        node_states, edges = inputs
+        x = self.preprocess(node_states)
+        for attention_layer in self.attention_layers:
+            x = attention_layer([x, edges]) + x
+        outputs = self.output_layer(x)
+        return outputs
+
+    def train_step(self, data):
+        indices, labels = data
+        with tf.GradientTape() as tape:
+            outputs = self([self.node_states, self.edges])
+            loss = self.compiled_loss(labels, tf.gather(outputs, indices))
+        grads = tape.gradient(loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.compiled_metrics.update_state(labels, tf.gather(outputs, indices))
+        return {m.name: m.result() for m in self.metrics}
+
+    def predict_step(self, data):
+        indices = data
+        outputs = self([self.node_states, self.edges])
+        return tf.nn.softmax(tf.gather(outputs, indices))
+
+    def test_step(self, data):
+        indices, labels = data
+        outputs = self([self.node_states, self.edges])
+        loss = self.compiled_loss(labels, tf.gather(outputs, indices))
+        self.compiled_metrics.update_state(labels, tf.gather(outputs, indices))
+        return {m.name: m.result() for m in self.metrics}
 
 
 def load_graph(pickle_path):
@@ -116,17 +248,74 @@ def main() :
     # DATASET   
     dataset_path = "../code/graph_dump/email_eu_core.pickle"
     data = load_graph(dataset_path)
+
     nodes_info = data['nodes_information']
-   
     unique_labels = list(set(nodes_info['true_label']))             # Values from 0 to 41 
     num_labels = len(unique_labels)
+    print(np.array(nodes_info['dg']).shape)
+    print(np.array(nodes_info['bv']).shape)
+    print(np.array(nodes_info['cl']).shape)
+    print(np.array(nodes_info['cc']).shape)
+    print(np.array(nodes_info['pred_label']).shape)
+
+    # TODO make it independent
+    node_states = np.stack([                                        # Shape: (num_nodes, num_features)  ---> each row a node, colums are features
+        np.array(nodes_info['dg']),
+        np.array(nodes_info['bv']),
+        np.array(nodes_info['cl']),
+        np.array(nodes_info['cc']),
+        np.array(nodes_info['pred_label'])
+    ], axis=-1)
+
+    edges = np.array(data['edges'])
+
+    
 
     
     # TRAIN TEST SPLIT - 80 20 rule
     data_split = split_data(data, TRAIN_SPLIT)
-    print(data['graph'].number_of_nodes())
-    print(len(data_split['test_indices']))
-    print(len(data_split['train_indices']))
+   
+
+
+    HIDDEN_UNITS = 100
+    NUM_HEADS = 4
+    NUM_LAYERS = 2
+    OUTPUT_DIM = len(num_labels)
+
+    NUM_EPOCHS = 50
+    BATCH_SIZE = 256
+    VALIDATION_SPLIT = 0.1
+    LEARNING_RATE = 3e-1
+    MOMENTUM = 0.9
+
+    loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    optimizer = keras.optimizers.SGD(LEARNING_RATE, momentum=MOMENTUM)
+    accuracy_fn = keras.metrics.SparseCategoricalAccuracy(name="acc")
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor="val_acc", min_delta=1e-5, patience=5, restore_best_weights=True
+    )
+
+    # Build model
+    gat_model = GraphAttentionNetwork(
+        node_states, edges, HIDDEN_UNITS, NUM_HEADS, NUM_LAYERS, OUTPUT_DIM
+    )
+
+    # Compile model
+    gat_model.compile(loss=loss_fn, optimizer=optimizer, metrics=[accuracy_fn])
+
+    gat_model.fit(
+        x=data_split['train_indices'],
+        y=data_split['train_labels'],
+        validation_split=VALIDATION_SPLIT,
+        batch_size=BATCH_SIZE,
+        epochs=NUM_EPOCHS,
+        callbacks=[early_stopping],
+        verbose=2,
+    )
+
+    _, test_accuracy = gat_model.evaluate(x=data_split['test_indices'], y=data_split['test_labels'], verbose=0)
+
+    print("--" * 38 + f"\nTest Accuracy {test_accuracy*100:.1f}%")
     
 
 

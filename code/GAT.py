52ae83f26e7e58ac6736584e3298fb5eb70e9cb7
zincalex <92792074+zincalex@users.noreply.github.com>
2025-01-03 16:43:40 +0100

    plots, new metrics, main overall

--- code/GAT.py
+++ code/GAT.py
@@ -1,45 +1,32 @@
-'''
-Zincuzzz questo ti serve per la cosa della GAT.
-Allora, ti do alcune info:
-    - il file .pickle che vedi dentro la cartella 'graph_dump' contiene il grafo con tutte le features:
-        - label : la vera label del nodo
-        - pred_label: la label predetta da node2vec + clustering
-        - dg: grado del nodo
-        - bv: betweenness centr del nodo
-        - cl: closeness centr del nodo
-        - cc: clustering coeff del nodo
-
-        
-Per caricare il grafo dal file .pickle ti basta fare 
-    G = pickle.load(open('filename.pickle', 'rb'))
-
-Ora se vuoi ottenere le info dal grafo (che è un nx.Graph) ti basta:
-
-ex. label
-    label = nx.get_node_attributes(G,"label")
-    label[i] #label del nodo num i
-
-Se hai dubbi, chiedimi o leggi la doc ( AI enjoyer :) )
-'''
-
-from gc import callbacks
-import pickle
-import numpy as np
-import stellargraph as sg
-from stellargraph import StellarGraph
-from stellargraph.mapper import FullBatchNodeGenerator
-from stellargraph.layer import GAT
-from stellargraph import datasets
-from sklearn import model_selection, preprocessing
-from tensorflow.keras import Model, optimizers, losses, metrics, layers 
-
-import matplotlib.pyplot as plt
-from tqdm.keras import TqdmCallback
-import pandas as pd
-import networkx as nx
 import os
 import argparse
+import pickle
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+from sklearn import model_selection, preprocessing
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from tensorflow.keras import Model, optimizers, losses, layers, metrics, callbacks
+from tqdm.keras import TqdmCallback
 
+import stellargraph as sg
+from stellargraph import StellarGraph, datasets
+from stellargraph.mapper import FullBatchNodeGenerator
+from stellargraph.layer import GAT
+
+# User defined libraries
+import plots
+
+
+# PARAMETERS
+DATASET = {"Cora" : datasets.Cora(), "CiteSeer" : datasets.CiteSeer(), "PubMedDiabetes" : datasets.PubMedDiabetes()}  
+SKIP_TRAINING = False
+TRAIN_SPLIT = 0.7
+VAL_SPLIT = 0.1
+EPOCHS = 1000
+HIDDEN_LAYERS = 8
+ATTENTION_HEADS = 6
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", type = str, help="Dataset chosen", default = 0)
@@ -58,83 +45,94 @@ class GraphAnalysis:
         self.graph = None
         self.labels = None
     
-    
+
     def load_graph(self, dataset, extra_features_path):
-        """Load graph from pickle file and extract node features"""
-        # LOAD FEATURES IF NEEDED 
+        """
+        Loads a graph from stellargraph dataset library and optionally updates it with additional node features.
+
+        Args:
+            dataset: The dataset object containing the graph and node subjects.
+            extra_features_path (str): Path to a pickle file containing additional node features.
+        """
         G, node_subjects = dataset.load()
 
+        # EXTRA FEATURES 
         if len(extra_features_path) != 0 : 
 
             with open(extra_features_path, 'rb') as file:
                 new_features = pickle.load(file)
 
-        
-
+            # Extract existing node features from the graph
             node_features = G.node_features()
+
+            # Combine existing features with the new features horizontally
             updated_features = np.hstack([node_features, new_features])
 
-            # Create pd.DataFrame needed for stellargraph
+            # Create a DataFrame for the updated node features
             node_data = pd.DataFrame(updated_features, index=G.nodes())  # Ensure node index is consistent
 
-            
             edges_list = G.edges()  # List of tuples (start, end)
-
-            # Create DataFrame
             edges_df = pd.DataFrame(edges_list, columns=["source", "target"])
             
+            # Create a new StellarGraph object
             G = StellarGraph(nodes=node_data, edges=edges_df)
-
 
         self.graph = G 
         self.labels = node_subjects
-        
-        
 
 
     def train_val_test_data_split(self, train_split, val_split):
-        """Split data into train/test sets"""
+        """
+        Splits the graph data into training, validation, and test sets.
+
+        Args:
+            train_split (float): Proportion of data to use for training.
+            val_split (float): Proportion of the remaining data to use for validation.
+
+        Returns:
+            tuple: Contains three DataFrames for training, validation, and test nodes.
+        """
         
         val_split = val_split / (1 - train_split)
-        nodes_indxs = self.labels.index
 
         # Use sklearn to split the data
         train_nodes, test_nodes = model_selection.train_test_split(self.labels, test_size=1-train_split, stratify=self.labels, random_state=42)
         val_nodes, test_nodes = model_selection.train_test_split(test_nodes, test_size=1-val_split, stratify=test_nodes, random_state=42)
         
         return train_nodes, val_nodes, test_nodes
-
-
-
     
-    def train_gat(self, train_nodes, val_nodes, test_nodes, epochs=10000, batch_size=32):
-        """Train GAT model"""
-        # Nodes: 916, Edges: 13993    26 unique labels  [ 1 21 14  9  4 17 34 11  5 10 36 37  7 22  8 15  3 20 16 38 13  6  0 35 23 19]
 
-        # Conversion to one-hot vectors --- remember here some labels are nomore, hence if problem look here
+    def train_gat(self, train_nodes, val_nodes, epochs, hidden_layers, attention_heads):
+        """
+        Trains a Graph Attention Network (GAT) model.
+
+        Args:
+            train_nodes: Training nodes and their labels.
+            val_nodes: Validation nodes and their labels.
+            epochs (int): Number of training epochs.
+            hidden_layers (int): Number of hidden layers in the GAT model.
+            attention_heads (int): Number of attention heads in the GAT model.
+
+        Returns:
+            tuple: Contains the trained GAT model and its training history.
+        """
+
+
+        # Conversion to one-hot vectors 
         target_encoding = preprocessing.LabelBinarizer()
         train_labels_onehot = target_encoding.fit_transform(train_nodes)          # Here we fit because we need to determine the lenght of the encoding
         val_labels_onehot = target_encoding.transform(val_nodes)
-        test_labels_onehot = target_encoding.transform(test_nodes)
-        
-
-        # The error occurs during the loss computation, where Keras tries to compute the categorical cross-entropy between the true labels and predicted labels.
-        print(f"Train labels shape (num nodes, tot labels): {train_labels_onehot.shape}")
-
-
-        # Create generators
+    
+        # Create generators for full-batch training ---> typically used from GAT
         generator = FullBatchNodeGenerator(self.graph, method="gat", sparse=False)
-        
         train_gen = generator.flow(train_nodes.index, train_labels_onehot)
         val_gen = generator.flow(val_nodes.index, val_labels_onehot )
-        test_gen = generator.flow(test_nodes.index, test_labels_onehot)
-        
-
+       
         # Create GAT model
         gat = GAT(
-            layer_sizes=[8, train_labels_onehot.shape[1]],
+            layer_sizes=[hidden_layers, train_labels_onehot.shape[1]],
             activations=['elu', 'softmax'],
-            attn_heads=8,
+            attn_heads=attention_heads,
             generator=generator,
             in_dropout=0.5,
             attn_dropout=0.5,
@@ -144,37 +142,66 @@ class GraphAnalysis:
         # Get input/output tensors from the GAT model
         x_inp, gat_output = gat.in_out_tensors()
 
-        # Add a Dense layer to map the GAT output to the correct number of classes
-        #predictions = layers.Dense(train_labels_onehot.shape[1], activation='softmax')(gat_output)
-        #print(f"Model predictions shape: {predictions.shape}")
-
         # Build and compile the model
         model = Model(inputs=x_inp, outputs=gat_output)
         model.compile(
-            optimizer=optimizers.Adam(learning_rate=0.005),
+            optimizer=optimizers.Adam(learning_rate=0.001),
             loss=losses.categorical_crossentropy,
             metrics=['acc']
         )
         
-        # Train
+        # Initialize Callbakcs
+        if not os.path.isdir("logs"):
+            os.makedirs("logs")
+        mc_callback = callbacks.ModelCheckpoint("logs/best_model.h5", monitor="val_acc", save_best_only=True, save_weights_only=True)
         tqdm_callback = TqdmCallback()
+
+        # TRAINING
         history = model.fit(
             train_gen,
             epochs=epochs,
             validation_data=val_gen,
             verbose=0, 
             shuffle=False,
-            callbacks = [tqdm_callback]
+            callbacks = [tqdm_callback, mc_callback]
         )
-        sg.utils.plot_history(history)
-        plt.show()
-
-        # Evaluate
-        test_metrics = model.evaluate(test_gen)
-        print(f"\nTest Accuracy: {test_metrics[1]:.4f}")
         
+        plots.plot_and_save_training_history(history, f"logs/{args.d}_training_history.png")
+
         return model, history
 
+
+    def test_gat(self, model, test_nodes, graph_labels) : 
+        """
+        Tests the trained GAT model on the test nodes.
+
+        Args:
+            model: The trained GAT model.
+            test_nodes: Test nodes and their labels.
+            graph_labels: All labels in the dataset for inverse transformation.
+
+        Returns:
+            tuple: Contains predicted labels and true labels for the test nodes.
+        """
+        # Labels one hot encoding
+        target_encoding = preprocessing.LabelBinarizer()
+        test_labels_onehot = target_encoding.fit_transform(test_nodes)
+
+        # Prediction
+        generator = FullBatchNodeGenerator(self.graph, method="gat", sparse=False)
+        test_gen = generator.flow(test_nodes.index, test_labels_onehot)
+        y_pred = model.predict(test_gen)
+
+        # Output conversion
+        y_pred = y_pred.squeeze()
+        y_pred_classes = np.argmax(y_pred, axis=1)
+        label_encoder = preprocessing.LabelEncoder()
+        label_encoder.fit(graph_labels)  # Fit on all labels in the dataset
+        y_pred_labels = label_encoder.inverse_transform(y_pred_classes)
+
+        y_true_labels = test_nodes.values
+
+        return y_pred_labels, y_true_labels
 
 
 
@@ -182,26 +209,35 @@ def main():
     # Disable warnings
     os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-    DATASET = {"Cora" : datasets.Cora(), "CiteSeer" : datasets.CiteSeer(), "PubMedDiabetes" : datasets.PubMedDiabetes()} 
     dataset = DATASET[args.d]
-
-    # PARAMETERS 
-    TRAIN_SPLIT = 0.7
-    VAL_SPLIT = 0.2
-    EPOCHS = 100
 
     # DATASET   
     extra_features_path = ""
     if args.e == 1 : 
         extra_features_path = "../code/graph_dump/" + args.d + ".pickle"
     
-
-
     analyzer = GraphAnalysis()
     analyzer.load_graph(dataset, extra_features_path)
     train_nodes, val_nodes, test_nodes = analyzer.train_val_test_data_split(TRAIN_SPLIT, VAL_SPLIT)
-    model, history = analyzer.train_gat(train_nodes, val_nodes, test_nodes, epochs=EPOCHS)
+    model, history = analyzer.train_gat(train_nodes, val_nodes, EPOCHS, HIDDEN_LAYERS, ATTENTION_HEADS)
+    y_pred_labels, y_true_labels = analyzer.test_gat(model, test_nodes, analyzer.labels)
+
+    # Calculate metrics
+    accuracy = accuracy_score(y_true_labels, y_pred_labels)
+    precision = precision_score(y_true_labels, y_pred_labels, average='weighted')
+    recall = recall_score(y_true_labels, y_pred_labels, average='weighted')
+    f1 = f1_score(y_true_labels, y_pred_labels, average='weighted')
+
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+
+    # Plot and save confusion matrix
+    class_names = np.unique(analyzer.labels)  # Get unique class names
+    plots.plot_and_save_confusion_matrix(y_true_labels, y_pred_labels, class_names, save_path=f"logs/{args.d}_confusion_matrix.png")
+
+
 
 if __name__ == '__main__':
     main()
---
+++ code/plots.py
@@ -0,0 +1,33 @@
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import stellargraph as sg
+import matplotlib.pyplot as plt
+
+def plot_and_save_confusion_matrix(y_true_labels, y_pred_labels, class_names, save_path="confusion_matrix.png"):
+        """
+        Plots and saves a confusion matrix.
+
+        Parameters:
+        - y_true_labels: True labels.
+        - y_pred_labels: Predicted labels.
+        - class_names: List of class names.
+        - save_path: Path to save the confusion matrix plot.
+        """
+        # Compute confusion matrix
+        cm = confusion_matrix(y_true_labels, y_pred_labels, labels=class_names)
+        
+        # Plot confusion matrix
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=class_names, yticklabels=class_names)
+        plt.title('Confusion Matrix')
+        plt.ylabel('True Label')
+        plt.xlabel('Predicted Label')
+        
+        # Save the plot
+        plt.savefig(save_path)
+        plt.show()
+
+def plot_and_save_training_history(history, plot_path) : 
+    sg.utils.plot_history(history)
+    plt.savefig(plot_path)
+    plt.close()
